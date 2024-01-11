d681a5efecfe35ee47f5d3bd35e9ac0bf8830ed6
Biki101 <thakurbiki52@gmail.com>
2024-01-11 16:57:14 +0545

    added template

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -34,93 +34,87 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Color.fromRGBO(54, 135, 147, 1),
         title: const Text(
           'Dashboard',
-          style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white),
+          style: TextStyle(
+              fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white),
         ),
         iconTheme: IconThemeData(color: Colors.white),
         elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: Container(
+        height: 50,
         child: ListView(
+          scrollDirection: Axis.horizontal,
           children: [
             Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(6),
-                color: Color.fromRGBO(54, 135, 147, 1),
-              ),
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-              child: Row(
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Total Patients',
-                        style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.white),
-                      ),
-                      Text(
-                        dashboardData != null ? '${dashboardData['total_patients']}' : '0',
-                        style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white),
-                      )
-                    ],
+                color: Colors.orange,
+                width: 100,
+                padding: EdgeInsets.all(10),
+                child: Center(
+                  child: Text(
+                    '2024-2-2',
+                    style: TextStyle(color: Colors.white),
                   ),
-                ],
-              ),
-            ),
+                )),
             Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(6),
-                color: Color.fromRGBO(86, 189, 91, 1),
-              ),
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-              child: Row(
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'New Registered Patients (Today)',
-                        style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.white),
-                      ),
-                      Text(
-                        dashboardData != null ? '${dashboardData['new_registered_patients']}' : '0',
-                        style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white),
-                      )
-                    ],
+                color: Colors.black,
+                width: 100,
+                padding: EdgeInsets.all(10),
+                child: Center(
+                  child: Text(
+                    '2024-2-2',
+                    style: TextStyle(color: Colors.white),
                   ),
-                ],
-              ),
-            ),
+                )),
             Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(6),
-                color: Color.fromRGBO(108, 117, 125, 1),
-              ),
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-              child: Row(
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Total Treatment Done (Today)',
-                        style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.white),
-                      ),
-                      Text(
-                        dashboardData != null ? '${dashboardData['treatment_done']}' : '0',
-                        style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white),
-                      )
-                    ],
+                color: Colors.black,
+                width: 100,
+                padding: EdgeInsets.all(10),
+                child: Center(
+                  child: Text(
+                    '2024-2-2',
+                    style: TextStyle(color: Colors.white),
                   ),
-                ],
-              ),
-            )
+                )),
+            Container(
+                color: Colors.black,
+                width: 100,
+                padding: EdgeInsets.all(10),
+                child: Center(
+                  child: Text(
+                    '2024-2-2',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )),
+            Container(
+                color: Colors.black,
+                width: 100,
+                padding: EdgeInsets.all(10),
+                child: Center(
+                  child: Text(
+                    '2024-2-2',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )),
+            Container(
+                color: Colors.black,
+                width: 100,
+                padding: EdgeInsets.all(10),
+                child: Center(
+                  child: Text(
+                    '2024-2-2',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )),
+            Container(
+                color: Colors.black,
+                width: 100,
+                padding: EdgeInsets.all(10),
+                child: Center(
+                  child: Text(
+                    '2024-2-2',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )),
           ],
         ),
       ),
