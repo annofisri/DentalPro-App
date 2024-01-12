0e042ec89a773cce9f8f01e2aaaed408740adccd
Biki101 <thakurbiki52@gmail.com>
2024-01-12 12:25:28 +0545

    added text field

--- lib/pages/patients.dart
+++ lib/pages/patients.dart
@@ -9,6 +9,8 @@ class PatientPage extends StatefulWidget {
 }
 
 class _PatientPageState extends State<PatientPage> {
+  TextEditingController patientName = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +25,37 @@ class _PatientPageState extends State<PatientPage> {
         elevation: 0,
       ),
       body: Center(
-        child: Text('Patients'),
+        child: Column(children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 300,
+                  decoration: BoxDecoration(
+                      border: Border(bottom: BorderSide(color: Colors.black))),
+                  child: TextField(
+                    controller: patientName,
+                    decoration: InputDecoration(
+                        hintText: 'Search for Patient',
+                        filled: true,
+                        fillColor: Colors.white,
+                        contentPadding: EdgeInsets.fromLTRB(12, 0, 12, 0),
+                        border: InputBorder.none),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+                  decoration: BoxDecoration(
+                      color: Color.fromRGBO(54, 135, 147, 1),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: Text('Find', style: TextStyle(color: Colors.white)),
+                )
+              ],
+            ),
+          ),
+        ]),
       ),
       drawer: NavDrawer(),
     );
