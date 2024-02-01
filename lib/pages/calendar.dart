8146cf56a5ed2728c2f1ec6fecc24dc53ddb75df
Biki101 <thakurbiki52@gmail.com>
2024-02-01 18:07:19 +0545

    added calendar to the calendar page

--- lib/pages/calendar.dart
+++ lib/pages/calendar.dart
@@ -1,5 +1,6 @@
 import 'package:dental/components/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
@@ -22,8 +23,41 @@ class _CalendarPageState extends State<CalendarPage> {
         iconTheme: IconThemeData(color: Colors.white),
         elevation: 0,
       ),
-      body: Center(
-        child: Text('Calendar Page'),
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+            decoration: BoxDecoration(
+                color: Color.fromRGBO(54, 135, 147, 1),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30))),
+            child: TableCalendar(
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: DateTime.now(),
+              calendarStyle: CalendarStyle(
+                  defaultTextStyle: TextStyle(color: Colors.white),
+                  weekNumberTextStyle: TextStyle(color: Colors.white),
+                  weekendTextStyle: TextStyle(color: Colors.white)),
+              headerStyle: HeaderStyle(
+                titleCentered: true,
+                formatButtonVisible: false,
+                titleTextStyle: TextStyle(
+                  color: Colors.white, // Set the color of the month text
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                leftChevronIcon: Icon(Icons.chevron_left, color: Colors.white),
+                rightChevronIcon:
+                    Icon(Icons.chevron_right, color: Colors.white),
+              ),
+              daysOfWeekStyle: DaysOfWeekStyle(
+                  weekdayStyle: TextStyle(color: Colors.white),
+                  weekendStyle: TextStyle(color: Colors.white)),
+            ),
+          ),
+        ],
       ),
       drawer: NavDrawer(),
     );
