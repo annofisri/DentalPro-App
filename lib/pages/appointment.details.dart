373eca179e954d44760f225f53972f675dc2cb4c
Biki101 <thakurbiki52@gmail.com>
2024-02-13 16:06:42 +0545

    updated appointment details page template

--- lib/pages/appointment.details.dart
+++ lib/pages/appointment.details.dart
@@ -30,7 +30,275 @@ class _AppointmentDetailsPageState extends State<AppointmentDetailsPage> {
         iconTheme: IconThemeData(color: Colors.white),
         elevation: 0,
       ),
-      body: Center(child: Text('Appointment Details')),
+      body: ListView(children: [
+        Container(
+          margin: EdgeInsets.fromLTRB(16, 10, 16, 0),
+          child: Row(
+            children: [
+              Icon(
+                Icons.image,
+                size: 40,
+              ),
+              SizedBox(
+                width: 6,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Dr. Nischal Thapa',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w700),
+                  ),
+                  Text(
+                    '- | -',
+                    style: TextStyle(fontSize: 13),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Container(
+          margin: EdgeInsets.fromLTRB(16, 0, 16, 0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    'Appointment No.',
+                    style: TextStyle(fontSize: 13),
+                  )),
+                  Expanded(
+                    child: Text('APN 0006', style: TextStyle(fontSize: 13)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    'Date',
+                    style: TextStyle(fontSize: 13),
+                  )),
+                  Expanded(
+                    child: Text('24/10/50', style: TextStyle(fontSize: 13)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    'Time',
+                    style: TextStyle(fontSize: 13),
+                  )),
+                  Expanded(
+                    child: Text('24/10/50', style: TextStyle(fontSize: 13)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    'Patient Name',
+                    style: TextStyle(fontSize: 13),
+                  )),
+                  Expanded(
+                    child: Text('24/10/50', style: TextStyle(fontSize: 13)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    'Patient Code',
+                    style: TextStyle(fontSize: 13),
+                  )),
+                  Expanded(
+                    child: Text('24/10/50', style: TextStyle(fontSize: 13)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    'Contact No.',
+                    style: TextStyle(fontSize: 13),
+                  )),
+                  Expanded(
+                    child: Text('24/10/50', style: TextStyle(fontSize: 13)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    'Address',
+                    style: TextStyle(fontSize: 13),
+                  )),
+                  Expanded(
+                    child: Text('24/10/50', style: TextStyle(fontSize: 13)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    'Chief Problem',
+                    style: TextStyle(fontSize: 13),
+                  )),
+                  Expanded(
+                    child: Text('24/10/50', style: TextStyle(fontSize: 13)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    'Treatment',
+                    style: TextStyle(fontSize: 13),
+                  )),
+                  Expanded(
+                    child: Text('24/10/50', style: TextStyle(fontSize: 13)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    'Treatment Time',
+                    style: TextStyle(fontSize: 13),
+                  )),
+                  Expanded(
+                    child: Text('24/10/50', style: TextStyle(fontSize: 13)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    'Buffer Time',
+                    style: TextStyle(fontSize: 13),
+                  )),
+                  Expanded(
+                    child: Text('24/10/50', style: TextStyle(fontSize: 13)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    'Note',
+                    style: TextStyle(fontSize: 13),
+                  )),
+                  Expanded(
+                    child: Text('24/10/50', style: TextStyle(fontSize: 13)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    'Status',
+                    style: TextStyle(fontSize: 13),
+                  )),
+                  Expanded(
+                    child: Text('24/10/50', style: TextStyle(fontSize: 13)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+            ],
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(16, 6, 16, 6),
+          decoration: BoxDecoration(color: Color.fromRGBO(238, 248, 239, 1)),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    'Created By',
+                    style: TextStyle(fontSize: 13),
+                  )),
+                  Expanded(
+                    child: Text('24/10/50', style: TextStyle(fontSize: 13)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    'Created Date',
+                    style: TextStyle(fontSize: 13),
+                  )),
+                  Expanded(
+                    child: Text('24/10/50', style: TextStyle(fontSize: 13)),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ]),
       drawer: NavDrawer(),
     );
   }
